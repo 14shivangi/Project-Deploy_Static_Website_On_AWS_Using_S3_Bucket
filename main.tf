bf26531dbac639b017ed477c566cc66bcdf30332
Shivangi Mishra <140191375+14shivangi@users.noreply.github.com>
2025-11-29 23:33:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,78 @@
 
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.23.0"
+    }
+    random = {
+        source = "hashicorp/random"
+        version = "3.7.2"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "random_id" "randomid" {
+    byte_length = 8
+}
+
+resource "aws_s3_bucket" "mybucket-website" {
+  bucket = "mybucket-website-${random_id.randomid.hex}"
+}
+
+resource "aws_s3_bucket_public_access_block" "mybucket-website" {
+  bucket = aws_s3_bucket.mybucket-website.id
+
+  block_public_acls = false
+  block_public_policy = false
+  ignore_public_acls = false
+  restrict_public_buckets = false
+
+}
+
+resource "aws_s3_bucket_policy" "mybucket-website" {
+  bucket = aws_s3_bucket.mybucket-website.id
+  policy = jsonencode(
+    {
+            Version = "2012-10-17",
+            Statement =[
+                {
+                Sid = "PublicReadGetObject",
+                Effect = "Allow",
+                Principal = "*",
+                Action = "s3:GetObject",
+                Resource = "${aws_s3_bucket.mybucket-website.arn}/*"
+                }
+            ]
+        }
+    )
+}
+
+resource "aws_s3_bucket_website_configuration" "mywebsite" {
+  bucket = aws_s3_bucket.mybucket-website.id
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+resource "aws_s3_object" "index_html" {
+  bucket       = aws_s3_bucket.mybucket-website.bucket
+  source       = "./index.html"
+  key          = "index.html"
+  content_type = "text/html"
+}
+
+resource "aws_s3_object" "styles_css" {
+  bucket       = aws_s3_bucket.mybucket-website.bucket
+  source       = "./style.css"
+  key          = "style.css"
+  content_type = "text/css"
+}
+
+output "WebsiteLink" {
+  value = aws_s3_bucket_website_configuration.mywebsite.website_endpoint
+}
